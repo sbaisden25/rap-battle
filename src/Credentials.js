3f77bd30d691bd383275dc1d8dbab7a5b4793227
sbaisden25 <spbaisden@gmail.com>
2022-05-03 20:58:39 -0400

    deploy

---
+++ src/Credentials.js
@@ -0,0 +1,9 @@
+const Credentials = () => {
+
+    return {
+        ClientId: 'b72cb1fefe474e2699b30d435a6b3187',
+        ClientSecret: '6996c29eda07419db995874cc981be6b'
+    }
+}
+
+export { Credentials };
